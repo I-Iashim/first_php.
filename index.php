55a422857a93df6f252232cbeb1e5ac5dda24fa8
I-Iashim <hashieditz786@gmail.com>
2025-07-17 17:59:16 +0500

    Update index.php

--- index.php
+++ index.php
@@ -18,19 +18,69 @@
     <?php
     // Variables
     $firstValue = "hello world <br>";
-    $secondValue = "How Are You..?";
+    $secondValue = "How Are You..? <br>";
     echo "$firstValue";
     echo $secondValue;
     ?>
     <?php
-    $name = "Physics Book";
-    $read = true;
-    if($read = true){
-        echo"you have read"." ".$name;
-    }
-    ?>
+    // Data Types
 
+    // String
+    $str = "This is a string <br>";
+    // Integer 
+    $intg = 123;
+    // Float
+    $float = 1.23;
+    // Bolean
+    $bolean = true;
+    // Null
+    $null = null;
+    // array
+    $arr = array("Physics","Computer","Maths");
+    // object
+    // $obj = new firstClass();
+    echo $str . "<br>";
+    echo $intg . "<br>";
+    echo $float . "<br>";
+    echo $bolean . "<br>";
+    echo $null . "<br>";
+    echo $arr[0] . "<br>";
+    var_dump($str);
+    var_dump($intg);
+    var_dump($float);
+    var_dump($bolean);
+    var_dump($null);
+    var_dump($arr);
+    ?>
+    <?php
+    // Constant Variables
+    define("value1",500);
+    define("value2",800);
+    echo "<br>" . value1 . "<br>";
+    echo value2 . "<br>";
+    ?>
+    <?php
+    // Arithmetic Operators
+    $a = 10;
+    $b = 15;
+    $c = $a + $b;
+    echo "$a + $b = $c <br>";
+    $c = $a - $b;
+    echo "$a - $b = $c <br>";
+    $c = $a * $b;
+    echo "$a * $b = $c <br>";
+    $c = $a ** $b;
+    echo "$a ** $b = $c <br>";
+    $c = $a % $b;
+    echo "$a % $b = $c <br>";
+    $c = $a / $b;
+    echo "$a / $b = $c <br>";
+    
+    
+    ?>
     
     
 </body>
 </html>
+    
+    
